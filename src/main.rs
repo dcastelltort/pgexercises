9351660c649ebec50dcc923893773f41ee719125
Damien Castelltort <dcastelltort@gmail.com>
2018-02-01 21:50:47 +0100

    feat(basic): added select where case

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use diesel::pg::PgConnection;
 use diesel::sql_query;
 use dotenv::dotenv;
 use std::env;
-
+use bigdecimal::BigDecimal;
 
 use models::*;
 
@@ -60,6 +60,24 @@ fn basic_select_specific() {
 
     let results_sql : Vec<FacilityPartial> = sql_query("SELECT name, membercost FROM facilities").load::<FacilityPartial>(&connection).expect("query failed to run");
     let results : Vec<FacilityPartial> = facilities.select((name, membercost)).load::<FacilityPartial>(&connection).expect("diesel operation failed");
+    
+    println!("\nSQL ---------");
+    print_results(&results_sql);
+    println!("\nDSL ---------");
+    print_results(&results);
+
+    assert_eq!(results_sql, results);
+}
+
+#[test]
+fn basic_select_where() {
+
+    use schema::facilities::dsl::*;
+
+    let connection = establish_connection();
+
+    let results_sql : Vec<Facility> = sql_query("SELECT * from facilities WHERE membercost > 0").load::<Facility>(&connection).expect("query failed to run");
+    let results : Vec<Facility> = facilities.filter(membercost.gt(BigDecimal::from(0))).get_results::<Facility>(&connection).expect("diesel operation failed");
     
     println!("\nSQL ---------");
     print_results(&results_sql);
